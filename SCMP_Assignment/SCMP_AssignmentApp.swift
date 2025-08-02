82c122dbaf8c52a958c80ec8becdf982b2f4c337
James Lau <andy771991673@gmail.com>
2025-08-02 15:12:33 +0800

    Initial Commit

---
+++ SCMP_Assignment/SCMP_AssignmentApp.swift
@@ -0,0 +1,32 @@
+//
+//  SCMP_AssignmentApp.swift
+//  SCMP_Assignment
+//
+//  Created by James on 2/8/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SCMP_AssignmentApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SCMP_AssignmentTests/SCMP_AssignmentTests.swift
@@ -0,0 +1,16 @@
+//
+//  SCMP_AssignmentTests.swift
+//  SCMP_AssignmentTests
+//
+//  Created by James on 2/8/2025.
+//
+
+import Testing
+
+struct SCMP_AssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
